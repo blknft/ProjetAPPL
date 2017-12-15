045fefe8896c8f3b9b30bf4b8ddb1e3943f8f544
oualidcheriaf <lololo75>
2017-12-15 10:28:54 +0100

    Moved to a new directory

---
+++ PAPPL/src/pappl/entitiesmongo/model/BPM.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pappl.entitiesmongo.model;
+
+import java.io.Serializable;
+
+/**
+ * @author oualidcheriaf
+ */
+
+public class BPM implements Serializable {
+
+    private String date;
+    private int valeur;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        return false;
+        // TODO: Warning - this method won't work in the case the id fields are not set
+    }
+
+    @Override
+    public String toString() {
+        return "pappl.entities.Temperature[ id=  ]";
+    }
+
+}
---
+++ PAPPL/src/pappl/entitiesmongo/model/Glycemie.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pappl.entitiesmongo.model;
+
+import java.io.Serializable;
+
+/**
+ * @author oualidcheriaf
+ */
+
+public class Glycemie implements Serializable {
+    private String date;
+    private Double valeur;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Double getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(Double valeur) {
+        this.valeur = valeur;
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        return false;
+        // TODO: Warning - this method won't work in the case the id fields are not set
+    }
+
+    @Override
+    public String toString() {
+        return "pappl.entities.Temperature[ id=  ]";
+    }
+
+}
